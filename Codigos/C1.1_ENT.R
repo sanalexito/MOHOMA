6b2be73d037dbb70af1ee8ed8046e304ab58fb40
Alejandro Sánchez Peralta <65984679+sanalexito@users.noreply.github.com>
2021-04-05 17:05:28 -0500

    Create C1.1_ENT.R

---
+++ Codigos/C1.1_ENT.R
@@ -0,0 +1,21 @@
+#Datos desagregados por entidad
+# Tiene lo de la reclasificación de la 5.4
+
+#rm(list=ls())
+library(survey)
+
+##########################################################################################
+#z<-c("Lectura de medidor", "Cuota fija","No pagan el agua","No sabe")
+z<-c("Localidades con menos de 2 500 habitantes","Localidades de 2500 o más habitantes")
+t7$TOT<-ifelse(!t7$factor%in%NA & t7$foliohog%in%1,t7$factor,0)
+t7$TOT_1<-ifelse(t7$ap_1%in%1 & t7$foliohog%in%1,t7$factor,0)
+t7$TOT_2<-ifelse(t7$ap_1%in%2 & t7$foliohog%in%1,t7$factor,0)
+
+asp <- svydesign(id=~UPM,strata=~EST,data=t7,weights=~1,nest = T)
+bla<-Tab_2(TOT,paste0("TOT_",1:2),t7$ENT,estados[-1])
+
+#####################################################################
+est<-bla[[1]]
+cv<- bla[[2]]
+int<-bla[[3]]
+se<- bla[[4]]
